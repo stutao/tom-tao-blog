e6f7f07336ed32d79554a6c7b312398886436d87
stutao <1450026690@qq.com>
2020-08-04 17:44:44 +0800

    文章发布与后端交互

--- src/components/utils/mdEdit.vue
+++ src/components/utils/mdEdit.vue
@@ -1,20 +1,15 @@
 <template>
   <div>
-    <mavon-editor
-      v-model="content"
-      ref="md"
-      @change="change"
-      style="min-height: 600px;min-width:400px"
-    />
-    <br>
+    <mavon-editor v-model="content" ref="md" @change="change" style="min-height: 600px" />
+    <br />
     <Button type="success" short @click="submit">发布</Button>
   </div>
 </template>
 
 <script>
 // 导入组件 及 组件样式
-import { mavonEditor } from 'mavon-editor'
-import 'mavon-editor/dist/css/index.css'
+import { mavonEditor } from "mavon-editor";
+import "mavon-editor/dist/css/index.css";
 export default {
   components: {
     mavonEditor,
@@ -23,33 +18,33 @@ export default {
   // html：及时转的html
   data() {
     return {
-      content: '',
-      html: '',
-    }
+      content: "",
+      html: "",
+    };
   },
   methods: {
     // 所有操作都会被解析重新渲染
     change(value, render) {
       // render 为 markdown 解析后的结果[html]
-      this.html = render
+      this.html = render;
     },
     // 提交
     submit() {
-      const articleTitle = this.$parent.$parent.$parent.title
-      const thumbnail_url = this.$parent.$parent.$parent.thumbnail_url
-      const tags = this.$parent.$parent.$parent.tags
-      const content = this.content
-      const html = this.html
+      const articleTitle = this.$parent.$parent.$parent.title;
+      const thumbnail_url = this.$parent.$parent.$parent.thumbnail_url;
+      const tags = this.$parent.$parent.$parent.tags;
+      const content = this.content;
+      const html = this.html;
       const data = {
         articleTitle,
         thumbnail_url,
         tags,
         content,
-        html
-      }
+      };
       console.log(data);
+      this.$axios.post("http://127.0.0.1:8000/article/create", data);
     },
   },
   mounted() {},
-}
+};
 </script>
